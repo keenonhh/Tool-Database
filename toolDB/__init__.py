6e921d5166c890d595e3d9c2e7462e345460e8c9
Keenon Hunsaker <hunsakke@oregonstate.edu>
2021-02-07 09:04:21 -0800

    Configuring secret key and database uri for heroku

--- toolDB/__init__.py
+++ toolDB/__init__.py
@@ -5,10 +5,10 @@ import os
 app = Flask(__name__)
 
 # secret key for use in wtforms
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 # set up database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
